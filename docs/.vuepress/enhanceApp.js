73bffc1fa54fd278fb94957efd367acada7f8b9f
VK <370725567@qq.com>
2024-03-24 12:47:03 +0800

    1

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,17 +1,24 @@
 export default ({ router }) => {
-	if (typeof process === 'undefined' || process.env.VUE_ENV !== 'server') {
-		router.onReady(() => {
-			const { app } = router;
-			app.$once("hook:mounted", () => {
-				setTimeout(() => {
-					const { hash } = document.location;
-					if (hash.length > 1) {
-						const id = decodeURIComponent(hash.substring(1));
-						const element = document.getElementById(id);
-						if (element) element.scrollIntoView();
-					}
-				}, 500);
-			});
-		});
-	}
+  if (typeof process === 'undefined') {
+    router.onReady(() => {
+      const { app } = router;
+      app.$once("hook:mounted", () => {
+        setTimeout(() => {
+          const { hash } = document.location;
+          if (hash.length > 1) {
+            const id = decodeURIComponent(hash.substring(1));
+            // const element = document.getElementById(id);
+            // if (element) element.scrollIntoView();
+            // 获取a标签元素
+            const anchor = document.querySelector(`#${id} .header-anchor`);
+            // 模拟点击
+            if (anchor) anchor.click();
+            setTimeout(() => {
+              if (anchor) anchor.click();
+            }, 400);
+          }
+        }, 500);
+      });
+    });
+  }
 }
